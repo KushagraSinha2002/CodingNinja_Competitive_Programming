a1d1a229af7a58bf6f57d7cc3cb2a0ce5b6365e1
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-04-26 14:48:24 +0530

    STL_Data_Structure

--- Language Tools/STL-DataStructures.cpp
+++ Language Tools/STL-DataStructures.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 #include<vector>
 #include<string>
+#include<utility> // Pair
+#include<set>
+
 
 using namespace std;
 
 int main()
 {
-    cout << "<==Vector==>" << endl << endl;
+    cout <<"------------------------";
+    cout << endl << "<== Vector ==>" << endl << endl;
     
     vector<int> v;
 
-    vector<int>:: iterator it;
+    vector<int>:: iterator it_vector;
     
     for(int i=0; i<5; i++){
         v.push_back(i+1);
     }
     
-    for(it=v.begin(); it != v.end(); it++){
-        cout<<*it<<endl;
+    for(it_vector=v.begin(); it_vector != v.end(); it_vector++){
+        cout<<*it_vector<<endl;
     }
     
-    cout << endl << "<==Vector==>" << endl;
+    cout << endl << "<== Vector ==>" << endl;
     
     cout <<"------------------------";
     
-    cout << endl<< "<==String==>" << endl<<endl;
+    cout << endl<< "<== String ==>" << endl<<endl;
     
     string s = "Kushagra";
     cout << s<<endl;
@@ -39,12 +43,52 @@ int main()
     cout << s3<< endl;
     
     if(s1.compare(s2) == 0){
-        cout << s1 <<" is equal to " << s2<<endl;
+        cout << "'"<< s1 <<"' is equal to '" << s2 << "'" << endl;
     }else{
-        cout << s1 <<" isn't equal to " << s2 << endl;
+        cout << "'"<< s1 <<"' isn't equal to '" << s2 << "'" << endl;
     }
     
-    cout << endl<< "<==String==>" << endl;
+    cout << endl<< "<== String ==>" << endl;
+    
+    cout <<"------------------------";
+    
+    cout << endl << "<== Pair ==>" << endl << endl;
+    
+    pair<int,char> p1;
+    pair<int,char> p2(1,'a');
+    
+    p1 = make_pair(2,'b');
+    
+    cout << p2.first<<" : "<<p2.second<<endl;
+    
+    cout << p1.first<<" : "<<p1.second<<endl;
+    
+    cout << endl << "<== Pair ==>" << endl;
+    
+    cout <<"------------------------";
+    
+    cout << endl << "<== Set ==>" << endl << endl;
+    
+    set<int> set1;
+    int arr[] = {1,2,3,4,5,6,5};
+    
+    for(int i=0; i<7; i++){
+        set1.insert(arr[i]);
+    }
+    
+    set<int>:: iterator it_set;
+    
+    for(it_set=set1.begin(); it_set != set1.end(); it_set++){
+        cout << *it_set<<endl;
+    }
+    
+    if(set1.find(6) == set1.end()){
+        cout<<"Element not found" << endl;
+    }else{
+        cout <<"Element found" << endl;
+    }
+    
+    cout << endl << "<== Set ==>" << endl;
     
     cout <<"------------------------";
 
