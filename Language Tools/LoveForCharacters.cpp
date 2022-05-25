a62b4ae70f18eb69e8a08091c7f949ea69abdacb
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-05-25 13:19:21 +0530

    Love for Characters

---
+++ Language Tools/LoveForCharacters.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<map>
+#include<cstring>
+#include <vector>
+using namespace std;
+
+void printOccurence(string s,int t){
+    map<char,vector<int>> m;
+    for(int i=0;i<t;i++){
+        m[s[i]].push_back(i);
+	}
+    cout<<m['a'].size() << " ";
+    cout<<m['s'].size() << " ";
+    cout<<m['p'].size();
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    string s;
+    cin >> s;
+    printOccurence(s,t);
+	return 0;
+}
