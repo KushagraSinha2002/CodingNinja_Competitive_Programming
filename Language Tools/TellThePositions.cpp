cf5a246afc1b6c45beceb4786a35e5b883150129
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-05-30 01:38:08 +0530

    Tell The Positions
    2 wrong test Case
    Will Update Soon

--- Language Tools/TellThePositions.cpp
+++ Language Tools/TellThePositions.cpp
@@ -1,71 +1,49 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
+#include<utility>
+#include<vector>
 using namespace std;
 
-class student{
-	public:
-		string name;
-		int marks;
-		int roll;
-};
-
-bool mycompare(student a, student b){
-
-	if (a.marks!=b.marks)
-	{
-		return (a.marks>b.marks);
-	}else{
-		return(a.roll<b.roll);
-	}
-	
+bool sortbysec(const pair<string,int> &a,
+              const pair<string,int> &b)
+{
+    if(a.second != b.second)
+    {
+		return (a.second > b.second);        
+	}else
+    {
+        return(a.second < b.second);
+    }
 }
 
-
-int main( int argc , char ** argv )
+struct student
 {
-	ios_base::sync_with_stdio(false) ; 
-	cin.tie(NULL) ; 
-	
-	int n;
-	std::cin>>n;
-
-	//std::vector<string> name;		//ncross1
-	std::vector<student> stud;			//ncross3
-
-	for (int i = 0; i < n; ++i)
-	{
-		string temp_name;
-		int sub1;
-		int sub2;
-		int sub3;
-		cin>>temp_name>>sub1>>sub2>>sub3;
-
-		int sum = sub1+sub2+sub3;
-
-		student temp_sub;
-
-		temp_sub.name = temp_name;
-		temp_sub.marks = sum;
-		temp_sub. roll = i+1;
-
-		stud.push_back(temp_sub);
-	}
-
-	
-
-	sort(stud.begin(), stud.end(), mycompare);
-
-	for (int i = 0; i < n; ++i)
-	{
-		cout << i+1<<" "<<stud.at(i).name<<'\n';
-		
-	}
-
-
-	
-
-	return 0 ; 
-
-
-
+    string name;
+    int totalMarks=0;
+};
+int main()
+{
+    int n;
+    cin>>n;
+    student studentArray[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>studentArray[i].name;
+        int marks;
+        for(int j=0;j<3;j++)
+        {
+            cin>>marks;
+            studentArray[i].totalMarks += marks;
+        }
+    }
+	vector< pair <string, int> > vect;
+	for(int i=0;i<n;i++)
+    {
+    	vect.push_back(make_pair(studentArray[i].name,studentArray[i].totalMarks));
+    }
+	sort(vect.begin(), vect.end(), sortbysec);
+    for(int i=0;i<n;i++)
+    {
+        cout <<i+1<<" "<<vect[i].first<<endl;
+    }
+    return 0;
 }
