7f952d5d23c5abd0835646a71efac08cd9df49a7
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-05-31 23:20:30 +0530

    Tell The Position

--- Language Tools/TellThePositions.cpp
+++ Language Tools/TellThePositions.cpp
@@ -3,47 +3,44 @@
 #include<vector>
 using namespace std;
 
-bool sortbysec(const pair<string,int> &a,
-              const pair<string,int> &b)
-{
-    if(a.second != b.second)
-    {
-		return (a.second > b.second);        
-	}else
-    {
-        return(a.second < b.second);
-    }
-}
-
 struct student
 {
     string name;
     int totalMarks=0;
+    int roll;
 };
+
+bool sortbysec(student *a,student *b)
+{
+    if(a->totalMarks != b->totalMarks)
+    {
+        return (a->totalMarks > b->totalMarks);        
+    }
+    return (a->roll<b->roll);
+}
+
+
 int main()
 {
     int n;
     cin>>n;
-    student studentArray[n];
+    student** studentArray=new student*[n];
     for(int i=0;i<n;i++)
     {
-        cin>>studentArray[i].name;
-        int marks;
-        for(int j=0;j<3;j++)
-        {
-            cin>>marks;
-            studentArray[i].totalMarks += marks;
-        }
+        studentArray[i]=new student();
     }
-	vector< pair <string, int> > vect;
-	for(int i=0;i<n;i++)
-    {
-    	vect.push_back(make_pair(studentArray[i].name,studentArray[i].totalMarks));
-    }
-	sort(vect.begin(), vect.end(), sortbysec);
     for(int i=0;i<n;i++)
     {
-        cout <<i+1<<" "<<vect[i].first<<endl;
+        cin>>studentArray[i]->name;
+        studentArray[i]->roll=i+1;
+        int marks1,m2,m3;
+        cin>>marks1>>m2>>m3;
+        studentArray[i]->totalMarks += (marks1+m2+m3);
+    }
+    sort(studentArray, studentArray+n, sortbysec);
+    for(int i=0;i<n;i++)
+    {
+        cout <<i+1<<" "<<studentArray[i]->name<<endl;
     }
     return 0;
 }
