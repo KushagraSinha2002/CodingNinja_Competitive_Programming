f1212f62411689aad05786c4aab2730ef31975f6
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-06-28 13:27:01 +0530

    XOR of Natural Numbers

--- Bit Manipulation/XORofNaturalNumbers.cpp
+++ Bit Manipulation/XORofNaturalNumbers.cpp
@@ -24,8 +24,53 @@ Sample Output:
 
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    
-    // write your code here
+
+int XORofNaturalNumbers(int n){
+    // If n is a multiple of 4
+    if (n % 4 == 0)
+        return n;
+
+    // If n%4 gives remainder 1
+    if (n % 4 == 1)
+        return 1;
+
+    // If n%4 gives remainder 2
+    if (n % 4 == 2)
+        return n + 1;
+
+    // If n%4 gives remainder 3
     return 0;
 }
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<XORofNaturalNumbers(n)<<endl;
+    }
+    return 0;
+}
+
+/* ------------------------------------ Logic -------------------------------------
+
+When we do XOR of numbers, we get 0 as the XOR value just before a multiple of 4.
+This keeps repeating before every multiple of 4. 
+
+                    Number Binary-Repr  XOR-from-1-to-n
+                    1         1           [0001]
+                    2        10           [0011]
+                    3        11           [0000]  <----- We get a 0
+                    4       100           [0100]  <----- Equals to n
+                    5       101           [0001]
+                    6       110           [0111]
+                    7       111           [0000]  <----- We get 0
+                    8      1000           [1000]  <----- Equals to n
+                    9      1001           [0001]
+                    10     1010           [1011]
+                    11     1011           [0000] <------ We get 0
+                    12     1100           [1100] <------ Equals to n
+
+  -----------------------------------------------------------------------------------
+*/
